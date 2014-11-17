765999d476804ad9c5619dc6c91b390476cf17b9
Venelin <Venelin@Venelin-PC.wireless.concordia.ca>
2014-11-17 11:37:37 -0500

    Added comments

--- src/adt/Node.java
+++ src/adt/Node.java
@@ -5,75 +5,102 @@ import interfaces.Position;
 /**
  * Class representing a Node object that holds a value and it's current Position
  * 
- * @author Anthony-Virgil Bermejo (6982166), Venelin Koulaxazov
+ * @author Anthony-Virgil Bermejo (6982166), Venelin Koulaxazov (6982425)
  */
 public class Node implements Position {
+
+	// member variable declarations
 	private char value;
 	private Node parent;
 	private int key;
 	private Node left;
 	private Node right;
-	
+
 	/**
-	 * Constructor
+	 * Constructor setting specified values.
 	 * 
-	 * @param position - Current Position
-	 * @param value - Value it will hold
+	 * @param key
+	 * @param parent
+	 * @param value
 	 */
-	public Node(int key, Node parent, char value)
-	{
+	public Node(int key, Node parent, char value) {
 		this.key = key;
 		this.parent = parent;
 		this.value = value;
 	}
-	
+
 	/**
 	 * Returns the value of this Node
 	 * 
 	 * @return value of the Position
 	 */
-	public char element() 
-	{
+	public char element() {
 		return value;
 	}
-	
+
 	/**
 	 * Returns the current Position of the Node within the Node List
 	 * 
 	 * @return current Position
 	 */
-	public int getKey()
-	{
+	public int getKey() {
 		return key;
 	}
-	
-	public Node getParent()
-	{
+
+	/**
+	 * Returns the parent of the current Node.
+	 * 
+	 * @return parent of the Node
+	 */
+	public Node getParent() {
 		return parent;
 	}
-	
-	public Node getLeft()
-	{
+
+	/**
+	 * Returns the left child of the current Node.
+	 * 
+	 * @return left child of the Node
+	 */
+	public Node getLeft() {
 		return left;
 	}
-	
-	public Node getRight()
-	{
+
+	/**
+	 * Returns the right child of the current Node.
+	 * 
+	 * @return right child of the Node
+	 */
+	public Node getRight() {
 		return right;
 	}
-	
-	public void setParent(Node parent)
-	{
+
+	/**
+	 * Sets the parent of the current Node.
+	 * 
+	 * @param parent
+	 *            new parent of the node
+	 */
+	public void setParent(Node parent) {
 		this.parent = parent;
 	}
-	
-	public void setLeft(Node left)
-	{
+
+	/**
+	 * Sets the left child of the current Node.
+	 * 
+	 * @param left
+	 *            the new left child of the Node
+	 */
+	public void setLeft(Node left) {
 		this.left = left;
 	}
-	
-	public void setRight(Node right)
-	{
+
+	/**
+	 * Sets the right child of the current Node.
+	 * 
+	 * @param right
+	 *            new right child of the Node
+	 */
+	public void setRight(Node right) {
 		this.right = right;
 	}
 }
