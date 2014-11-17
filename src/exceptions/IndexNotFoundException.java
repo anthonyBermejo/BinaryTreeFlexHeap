723281db2c817bf0fef50534460d749806ffade1
Venelin <Venelin@Venelin-PC.wireless.concordia.ca>
2014-11-17 14:14:35 -0500

    New package for exceptions; IndexNotFoundException for ArrayList

---
+++ src/exceptions/IndexNotFoundException.java
@@ -0,0 +1,30 @@
+package exceptions;
+
+/**
+ * Custom exception for when an index is not found within an ArrayList
+ * 
+ * @author Anthony-Virgil Bermejo (6982166), Venelin Koulaxazov (6982425)
+ * @version 1.0
+ */
+public class IndexNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 2420191089813860473L;
+
+	/**
+	 * Constructs an IndexNotFoundException with "Index not found." as its error
+	 * message.
+	 */
+	public IndexNotFoundException() {
+		super("Index not found.");
+	}
+
+	/**
+	 * Constructs a IndexNotFoundException with the specified message.
+	 * 
+	 * @param message
+	 *            the error message.
+	 */
+	public IndexNotFoundException(String message) {
+		super(message);
+	}
+}
