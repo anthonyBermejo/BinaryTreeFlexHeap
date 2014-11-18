0a90504e2d573a8de164bd3169018c7aacd5ec1a
Venelin <Venelin@192.168.2.23>
2014-11-18 13:15:53 -0500

    Commented FlexHeap

--- src/adt/FlexHeap.java
+++ src/adt/FlexHeap.java
@@ -7,6 +7,9 @@ package adt;
  */
 public class FlexHeap {
 
+	/*
+	 * Defines the types of heap to be implemented
+	 */
 	private enum HeapType {
 		MIN_HEAP, MAX_HEAP
 	}
@@ -24,34 +27,36 @@ public class FlexHeap {
 	}
 
 	/**
+	 * Removes and returns the element with the smallest or biggest key value
+	 * depending on the heap status. Repairs the heap afterwards.
 	 * 
-	 * @return
+	 * @return the removed element
 	 */
 	public int remove() {
 		return 0;
 	}
 
 	/**
-	 * 
+	 * Transforms a min-heap to a max-heap if applicable.
 	 */
-	public void toggleHeap() {
-		if (HeapType.MIN_HEAP == heapType)
-			switchMaxHeap();
-		else
-			switchMinHeap();
+	public void switchMaxHeap() {
+
 	}
 
 	/**
-	 * 
+	 * Transforms a max-heap to a max-heap if applicable.
 	 */
 	public void switchMinHeap() {
 
 	}
 
 	/**
-	 * 
+	 * Transforms a min- to a max-heap or vice versa.
 	 */
-	public void switchMaxHeap() {
-
+	public void toggleHeap() {
+		if (HeapType.MIN_HEAP == heapType)
+			switchMaxHeap();
+		else
+			switchMinHeap();
 	}
 }
