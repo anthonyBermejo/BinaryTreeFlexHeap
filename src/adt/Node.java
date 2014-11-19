abbf8088761b5c24b8a2192dadababe363630028
Anthony-Virgil Bermejo <anthonyvirgil.bermejo@gmail.com>
2014-11-19 11:15:09 -0500

    Hello

--- src/adt/Node.java
+++ src/adt/Node.java
@@ -97,7 +97,7 @@ public class Node implements Position {
 	 * Sets the key of the current Node.
 	 * 
 	 * @param key
-	 *            the new key of the node.
+	 *            the new key of the Node.
 	 */
 	public void setKey(int key) {
 		this.key = key;
